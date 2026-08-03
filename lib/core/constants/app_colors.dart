90f22b9e7e9152149227b5b407606032ded75ae5
Mr.park <20261107@dsm.hs.kr>
2026-08-04 01:20:03 +0900

    feat: 코드이전
    원래 있던 레포의 코드를 옮김

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color point = Color(0xff21F1A8);
+  static const Color dark = Color(0xff171717);
+  static const Color light = Color(0xffFAFAFA);
+}
